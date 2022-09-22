e3328be5a0fcf4601671626f6895d37f0763a61f
Nguyễn Minh Tuấn <59287970+prostatus123@users.noreply.github.com>
2022-09-22 15:32:58 +0700

    update code

---
+++ Singleton/src/main/java/com/zack/design/pattern/Singleton/model/SingletonMain.java
@@ -0,0 +1,29 @@
+package com.zack.design.pattern.Singleton.model;
+
+public class SingletonMain {
+
+    public static void main(String[] args) {
+
+        Singleton x = Singleton.getInstance();
+
+        Singleton y = Singleton.getInstance();
+
+        Singleton z = Singleton.getInstance();
+
+        System.out.println("Hashcode of x is "
+                + x.hashCode());
+        System.out.println("Hashcode of y is "
+                + y.hashCode());
+        System.out.println("Hashcode of z is "
+                + z.hashCode());
+
+
+        if (x == y && y == z) {
+            System.out.println("Three object point to the same memory location on the heap i.e, to the same object");
+        }else {
+            System.out.println("There objects DO NOT point to the same memory location on the heap");
+        }
+
+
+    }
+}
